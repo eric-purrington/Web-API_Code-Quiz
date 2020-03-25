b652f49e7a63dd62364e46fcd5abf72ae7cfb8d0
Seth Purr <eric.purrington@gmail.com>
2020-03-24 22:07:36 -0700

    js and html changes

--- script.js
+++ script.js
@@ -1,49 +1,79 @@
 var questions = [{
-    question: "I am a question?",
-    answers: ["1. I am wrong", "2. I am right", "3. I am wrong", "4. I am right"],
-    correctAnswer: "2. I am right",
+    question: "His Dark Materials: What is Dust?",
+    answers: ["1. Fine particles of solid matter", "2. Elementary particles associated with consciousness", "3. Particles that humans are made out of", "4. Paricles that are attracted to sin"],
+    correctAnswer: "2. Elementary particles associated with consciousness",
 }, 
     {
-    question: "I am a question?",
-    answers: ["1. I am wrong", "2. I am wrong", "3. I am right", "4. I am wrong"],
-    correctAnswer: "3. I am right",
+    question: "The Lord of the Rings: How many rings of power are there?",
+    answers: ["1. One", "2. Nine", "3. Twenty", "4. Twenty-One"],
+    correctAnswer: "3. Twenty",
 }, 
     {
-    question: "I am a question?",
-    answers: ["1. I am right", "2. I am wrong", "3. I am wrong", "4. I am wrong"],
-    correctAnswer: "1. I am right",
+    question: "Star Wars: How fast can the Millennium Falcon make the Kessel Run?",
+    answers: ["1. Less than twelve parsecs", "2. Less than nineteen parsecs", "3. Exactly twenty-two parsecs", "4. The Kessel Run can not be accessed by Resistance Scum"],
+    correctAnswer: "1. Less than twelve parsecs",
 }, 
     {
-    question: "I am a question?",
-    answers: ["1. I am wrong", "2. I am wrong", "3. I am wrong", "4. I am right"],
-    correctAnswer: "4. I am right",
+    question: "Marvel: What do the Avengers eat after the Battle of New York?",
+    answers: ["1. Falafel", "2. Spaghetti", "3. Pizza", "4. Shawarma"],
+    correctAnswer: "4. Shawarma",
 }, 
     {
-    question: "I am a question?",
-    answers: ["1. I am right", "2. I am wrong", "3. I am wrong", "4. I am wrong"],
-    correctAnswer: "1. I am right"
+    question: "Harry Potter: The fifth Harry Potter title reads: Harry Potter and the Order of the ________.",
+    answers: ["1. Bees", "2. Snake", "3. Pheonix", "4. Half-Blood Prince"],
+    correctAnswer: "3. Pheonix"
 }
 ];
-var checker = document.getElementById("checker");
 var blockContainer = document.getElementById("blockContainer");
-var startBtn = document.createElement("button");
+var timeEl = document.getElementById("time");
+var hsEl = document.getElementById("viewHs");
 var currentQ = 0;
+secondsLeft = 60;
 
-
+// Calls first page function immediately
 startPage();
 
+// Start page function
 function startPage() {
+    // Creating the variables for each element on the Start Page
     var quizTitle = document.createElement("h1");
     var quizExp = document.createElement("p");
-    quizTitle.textContent = "Code Quiz Challenge";
-    quizExp.textContent = "I explain the quiz.";
+    var startBtn = document.createElement("button");
+
+    // Putting content into those elements
+    quizTitle.textContent = "Fantasy Worlds Quiz";
+    quizExp.textContent = "Come one, come all! Test your knowledge on Eric's favorite fantasy worlds! 60 seconds on the clock! 5 seconds will be deducted with each wrong answer! Good luck...";
     startBtn.textContent = "Start Quiz";
+    
+    // Appending those children to their parent
     blockContainer.appendChild(quizTitle);
     blockContainer.appendChild(quizExp);
     blockContainer.appendChild(startBtn);
+    
+    // Start button starts the question function and timer
     startBtn.addEventListener("click", questionBlock);
+    startBtn.addEventListener("click", setTime);
 }
 
+// Timer function taken from class activity with minor changes
+function setTime() {
+    
+    // Sets interval and starts the countdown (per 1000ms)
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+
+        // Adding content to timer at top right of page
+        timeEl.textContent = "Time: " + secondsLeft;
+
+        // If we are on the 6th element (doesn't exist) of the questions array then it will stop the time and start the initials page function
+        if (currentQ === 5) {
+            clearInterval(timerInterval);
+            initialsPage();
+        }
+    }, 1000);
+}
+
+// This function cycles through the questions erasing the questions before
 function questionBlock(event) {
     blockContainer.innerHTML = "";
     var question = document.createElement("h2");
@@ -61,19 +91,24 @@ function questionBlock(event) {
         ansBtns[i].onclick = function ansCheck(event) {
             var hr = document.createElement("hr");
             var ansResp = document.createElement("h3");
+
             if (this.textContent == questions[currentQ].correctAnswer) {
                 ansResp.textContent = "Correct!";
                 currentQ++;
             } else {
                 ansResp.textContent = "Wrong!";
                 currentQ++;
+                secondsLeft -= 5;
             }
 
-            setTimeout(function(){ checker.appendChild(hr);
-                checker.appendChild(ansResp);
+            setTimeout(function () {
+                blockContainer.appendChild(hr);
+                blockContainer.appendChild(ansResp);
             }, 0);
 
-            setTimeout(function(){ checker.textContent = "";
+            setTimeout(function () {
+                blockContainer.removeChild(hr);
+                blockContainer.removeChild(ansResp);
             }, 1000);
 
             if (currentQ == 5) {
@@ -87,18 +122,25 @@ function questionBlock(event) {
 
 function initialsPage() {
     blockContainer.innerHTML = "";
-    // stoptimer
     var allDone = document.createElement("h1");
     var finScore = document.createElement("p");
+
+    // !!!! THESE BOTH NEED TO ME IN A FUCKING FORM
     var submitBtn = document.createElement("button");
-    var initialsInput = document.createElement("form");
+    var initialsInput = document.createElement("input");
+
+    initialsInput.setAttribute("type", "text");
     allDone.textContent = "All done!";
-    finScore.textContent = "Your score was " + time + "!";
+    finScore.textContent = "Your score was " + secondsLeft + "!";
     submitBtn.textContent = "Submit";
+
     blockContainer.appendChild(allDone);
     blockContainer.appendChild(finScore);
     blockContainer.appendChild(submitBtn);
     blockContainer.appendChild(initialsInput);
+
+    localStorage.setItem("Score", secondsLeft);
+    localStorage.setItem("initials", initialsInput.value);
     submitBtn.addEventListener("click", highscorePage);
 }
 
@@ -106,10 +148,23 @@ function highscorePage(event) {
     blockContainer.innerHTML = "";
     var hsHead = document.createElement("h1");
     var hsList = document.createElement("ol");
-    var initials = document.createElement("li");
+    var initialsAndScore = document.createElement("li");
     var goBack = document.createElement("button");
     var clearHS = document.createElement("button");
 
+    hsHead.textContent = "Highscores";
+    clearHS.textContent = "Clear HighScores";
+    goBack.textContent = "Restart";
+    var initials = localStorage.getItem("initials");
+    var score = localStorage.getItem("Score");
+    initialsAndScore.textContent = initials + "-" + score;
+
+    blockContainer.appendChild(hsHead);
+    blockContainer.appendChild(hsList);
+    hsList.appendChild(initialsAndScore);
+    blockContainer.appendChild(clearHS);
+    blockContainer.appendChild(goBack);
+    
     clearHS.onclick = function (event) {
         hsList.textContent = "";
     }
